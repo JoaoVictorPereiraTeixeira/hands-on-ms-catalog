f41ee7fd6372b69efa27f9aea305a075ea66f1e7
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2021-12-16 11:02:12 -0300

    Armazenando primeira sincronização de relacionamentos no elasticsearch

---
+++ api-catalog-videos/src/commands/fixtures.commands.ts
@@ -0,0 +1,56 @@
+// TODO
+
+import {DefaultCrudRepository} from '@loopback/repository';
+import {default as chalk} from 'chalk';
+import {Client} from 'es6';
+import {MicroCatalogApplication} from '..';
+import * as config from '../../config';
+import {Esv7DataSource} from '../datasources';
+import fixtures from '../fixtures';
+
+
+export class FixtureCommands {
+  static command = 'fixture'
+  static description = 'Fixture data in ElasticSearch'
+  app: MicroCatalogApplication;
+
+
+  async run(){
+    await this.bootApp();
+    console.log(chalk.green('Delete all documents'))
+    await this.deleteAllDocuments();
+
+    for(const fixture of fixtures){
+      const repository = this.getRepository<DefaultCrudRepository<any,any>>(fixture.model)
+      await repository.create(fixture.fields)
+    }
+
+    console.log(chalk.green('Documents generated'))
+  }
+
+  private async bootApp(){
+    this.app = new MicroCatalogApplication(config);
+    await this.app.boot();
+  }
+
+  private async deleteAllDocuments(){
+    const datasource: Esv7DataSource = this.app.getSync<Esv7DataSource>('datasources.esv7')
+    //@ts-ignore
+    const index = datasource.adapter.settings.index;
+    //@ts-ignore
+    const client: Client = datasource.adapter.db;
+    await client.delete_by_query({
+      index,
+      body: {
+        query: {match_all: {}}
+      }
+    })
+  }
+
+  private getRepository<T>(modelName: string): T{
+    return this.app.getSync(`repositories.${modelName}Repository`)
+  }
+
+}
+
+
